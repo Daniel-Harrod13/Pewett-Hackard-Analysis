311ac5a0588612290b29759eba04d6171ceedd4e
Daniel-Harrod13 <90801814+Daniel-Harrod13@users.noreply.github.com>
2021-11-07 22:33:18 -0700

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -42,7 +42,7 @@ FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 );
 CREATE TABLE titles(
   emp_no INT NOT NULL,
-  salary INT NOT NULL,
+  title VARCHAR NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
